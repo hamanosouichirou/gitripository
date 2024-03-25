479155f07c99f849683e1370148efcb9babd6bfa
hamanosouichirou <souichirou199724@gmail.com>
2024-03-25 21:17:01 +0900

    ブログ投稿作成画面のDeleteボタン実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,7 +39,12 @@ class PostController extends Controller
     {
     $input_post = $request['post'];
     $post->fill($input_post)->save();
-
     return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
